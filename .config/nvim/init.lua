a18cfb34c7b17d0165a768f9a781163ed2553ade
hisyamsk <hisyamskurniawan18@gmail.com>
2024-04-01 18:38:28 +0700

    feat: add rust lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -697,7 +697,7 @@ require('lazy').setup({
         gopls = {},
         pyright = {},
         dockerls = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
